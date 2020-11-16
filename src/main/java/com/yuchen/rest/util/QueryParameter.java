a874c492e574c745f8b61c5b77facbc7437a1d40
yuchenpan <yuchenpan88@gmail.com>
2020-11-16 17:51:04 +0000

    Add static factories to QueryParameter to avoid having to pass in Strings.

--- src/main/java/com/yuchen/rest/util/QueryParameter.java
+++ src/main/java/com/yuchen/rest/util/QueryParameter.java
@@ -1,9 +1,21 @@
 package com.yuchen.rest.util;
 
+import com.yuchen.rest.model.instrument.candle.CandlestickGranularity;
+
 public record QueryParameter(String name, String value) {
     public static QueryParameter of(String name, String value)
     {
         return new QueryParameter(name, value);
+    }
+
+    public static QueryParameter candlestickGranularity(CandlestickGranularity granularity)
+    {
+        return new QueryParameter("granularity", granularity.name());
+    }
+
+    public static QueryParameter count(int count)
+    {
+        return new QueryParameter("count", Integer.toString(count));
     }
 
     @Override
