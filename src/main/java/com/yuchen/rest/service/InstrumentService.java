a23b35af830d95b3932d426acb8185088b8fdd5a
yuchenpan <yuchenpan88@gmail.com>
2020-11-26 13:19:17 +0000

    Add docs to instrument service

--- src/main/java/com/yuchen/rest/service/InstrumentService.java
+++ src/main/java/com/yuchen/rest/service/InstrumentService.java
@@ -15,18 +15,28 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 
+/**
+ * A service to access all the instrument endpoints offered by OANDA.
+ */
 public class InstrumentService {
     private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
 
     private final OANDAConnection oandaConnection;
     private final ObjectMapper jsonMapper;
 
-    InstrumentService(OANDAConnection oandaConnection, ObjectMapper jsonMapper)
-    {
+    InstrumentService(OANDAConnection oandaConnection, ObjectMapper jsonMapper) {
         this.oandaConnection = oandaConnection;
         this.jsonMapper = jsonMapper;
     }
 
+    /**
+     * Gets candlestick data for an instrument.
+     *
+     * @param instrument The instrument to get candlestick data for.
+     * @param parameters Parameters for the query. For example the timeframe the candles are in and the number of
+     *                   candles to get.
+     * @return Candlestick data for given instrument and parameters.
+     */
     public Candles candles(Instrument instrument, List<QueryParameter> parameters) {
         URI uri = URIBuilder.api(oandaConnection.api().endpoint())
                 .endpoint(Endpoints.candles(instrument))
@@ -41,8 +51,7 @@ public class InstrumentService {
         try {
             HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
             return jsonMapper.readValue(response.body(), Candles.class);
-        } catch (InterruptedException | IOException e)
-        {
+        } catch (InterruptedException | IOException e) {
             throw new RuntimeException(e);
         }
     }
