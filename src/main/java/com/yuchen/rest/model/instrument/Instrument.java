a03c056eca190419bdc0c55a68e448ede87f13fa
yuchenpan <yuchenpan88@gmail.com>
2020-11-28 14:12:31 +0000

    Add major pairs to instruments

--- src/main/java/com/yuchen/rest/model/instrument/Instrument.java
+++ src/main/java/com/yuchen/rest/model/instrument/Instrument.java
@@ -3,12 +3,17 @@ package com.yuchen.rest.model.instrument;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Instrument {
-    EURUSD("EUR_USD");
+    EURUSD("EUR_USD"),
+    USDJPY("USD_JPY"),
+    GBPUSD("GBP_USD"),
+    USDCHF("USD_CHF"),
+    USDCAD("USD_CAD"),
+    AUDUSD("AUD_USD"),
+    NZDUSD("NZD_USD");
 
     private final String name;
 
-    Instrument(String name)
-    {
+    Instrument(String name) {
         this.name = name;
     }
 
